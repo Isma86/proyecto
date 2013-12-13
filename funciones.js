27220f3cfd274d7b924d710ef71dd10ec92ded3d
unknown <ismaellara86@gmail.com>
2013-12-13 17:14:40 +0100

    'proyecto'

---
+++ funciones.js
@@ -0,0 +1,104 @@
+﻿$(document).ready(function() {
+
+	$('#envio_datos').click(function(){
+
+		var url = 'http://localhost:8080/';
+		
+		var nombre = $('#coger_nombre').val();
+		var apellido = $('#coger_apellido').val();	
+		var telefono = $('#coger_telefono').val();
+		
+		$.ajax({
+		   url: '/contactos/' + nombre + '/' + apellido + '/' + telefono,
+		   type: 'PUT',
+		   success: function(data) {
+				alert(data);
+		   }
+		});
+
+		$('#envio_datos').css('display', 'none');
+		$('#confirmo_datos').css('display', 'block');
+	});
+	
+	$('#confirmo_datos').click(function(){
+		
+		var url = 'http://localhost:8080/';
+		
+		var nombre = $('#coger_nombre').val();
+		var apellido = $('#coger_apellido').val();	
+		var telefono = $('#coger_telefono').val();
+		
+		$.post(url + 'contactos/' + nombre + '/' + apellido + '/' + telefono, function( data) {
+			alert(data);
+		});
+		
+		$('#coger_nombre').val('');
+		$('#coger_apellido').val('');	
+		$('#coger_telefono').val('');
+		$('#confirmo_datos').css('display', 'none');
+		$('#envio_datos').css('display', 'block');
+		
+	});
+	
+	
+	$('#mostrar_agenda').click(function(){
+
+		var url = 'http://localhost:8080/';
+		window.location = url + 'agenda/contactos';
+		
+	});
+	
+	$('#buscar_datos').click(function(){
+
+		var url = 'http://localhost:8080/';
+
+		$.get(url + 'agenda/' + $('#buscar_nombre').val(), function(data){
+		
+			if ( data == $('#buscar_nombre').val() + " no encontrado"){
+				$('#buscar_nombre').val('');
+				$('#muestra_nombre').val('');
+				$('#datos_nom').text('');
+				$('#muestra_apellido').val('');
+				$('#datos_ap').text('');
+				$('#muestra_telefono').val('');
+				$('#datos_tel').text('');
+				$('#no_contacto_buscar').val('ESTE CONTACTO NO EXISTE EN NUESTRA AGENDA');
+			}else{
+				$('#buscar_nombre').val('');
+				$('#no_contacto_buscar').val('');
+				$('#datos_nom').text('Nombre => ');
+				$('#muestra_nombre').val(data.nombre);
+				$('#datos_ap').text('Apellido => ');
+				$('#muestra_apellido').val(data.apellido);
+				$('#datos_tel').text('Telefono => ');
+				$('#muestra_telefono').val(data.telefono);
+			}
+			
+		});
+	});
+	
+	$('#eliminar_contacto').click(function(){
+
+		var url = 'http://localhost:8080/';
+		
+		$.ajax({
+		   url: url + 'agenda/' +  $('#eliminar_nombre').val(),
+		   type: 'DELETE',
+		   success: function(data) {
+				
+			if ( data == $('#eliminar_nombre').val() + " no encontrado"){
+				$('#eliminar_nombre').val('');
+				$('#no_contacto_eliminar').val('ESTE CONTACTO NO EXISTE EN NUESTRA AGENDA');
+			}else{
+				$('#eliminar_nombre').val('');
+				$('#no_contacto_eliminar').val('CONTACTO ELIMINADO');
+			}
+		   }
+		});
+
+	});
+	
+});
+
+
+
---
+++ servidor-contactos.js
@@ -0,0 +1,99 @@
+﻿var express=require('express');
+var fs = require('fs');
+
+var persona = new Array();
+var contactos = new Object(persona);
+
+var app = express();
+var puerto = process.argv[2]?process.argv[2]:8080;
+var portada = fs.readFileSync('index.html','utf8');
+
+app.get('/', function (req, res) { 
+    res.send(portada);
+});
+
+app.get('/:page', function (req, res) { 
+    var js = fs.readFileSync(req.params.page);
+    res.contentType('text/javascript');
+    res.send(js);
+});
+
+app.put('/contactos/:nombre/:apellido/:telefono', function( req, res ) {
+	
+	persona[0] = "nombre";
+	persona[1] = "apellido";
+	persona[2] = "telefono";
+	
+	contactos.nombre = req.params.nombre;
+	contactos.apellido = req.params.apellido;
+	contactos.telefono = req.params.telefono;
+	
+	res.send('Se ha creado la persona ' + req.params.nombre + ' con apellido ' + req.params.apellido + ' y telefono ' + req.params.telefono);
+
+});
+
+app.get('/agenda/contactos', function (req, res) {
+	
+	//VAMOS A MOSTRAR LOS CONTACTOS DE MANERA ORDENADA
+	var resultados = '';
+	contactos.forEach(function(datos){
+		
+		if(datos.nombre !== undefined){
+			resultados += ("<p>El nombre del contacto: " + datos.nombre + ' ' + datos.apellido + " con teléfono: " + datos.telefono + "</p>");
+		}
+		
+	});
+	
+	res.send(resultados);
+	
+});
+
+app.get('/agenda/:nombre', function (req, res){
+
+	var existe = false;
+
+	contactos.forEach(function(datos){
+		if(datos.nombre == req.params.nombre){
+			existe = true;
+			res.send(datos); 
+		}
+	});
+	//CONTROLAMOS SI EL CONTACTO ESTA METIDO
+	if(!existe){
+		res.send(req.params.nombre + " no encontrado");
+	}
+
+});
+
+app.delete('/agenda/:nombre', function (req, res){
+
+	var existe = false;
+
+	contactos.forEach(function(datos){
+		if(datos.nombre == req.params.nombre){
+			existe = true;
+			delete datos.nombre;
+			delete datos.apellido;
+			delete datos.telefono;
+			res.send(datos); 
+		}
+	});
+	//CONTROLAMOS SI EL CONTACTO ESTA METIDO
+	if(!existe){
+		res.send(req.params.nombre + " no encontrado");
+	}
+
+});
+
+
+app.post('/contactos/:nombre/:apellido/:telefono', function (req, res) { 		
+	
+	datos = {nombre : req.params.nombre, apellido : req.params.apellido, telefono : req.params.telefono};
+	contactos.push(datos);
+	res.send('Se ha añadido la persona a la agenda');
+    console.log( { 'Post':  contactos} );
+});
+
+app.listen(puerto);
+
+console.log('Server running at http://127.0.0.1:'+puerto+'/');
